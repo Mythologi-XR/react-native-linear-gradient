7b2c18670cac6f0679fecf31ff4cb6851f69de3e
Michael Hemingway <mikemingway@gmail.com>
2022-03-17 22:53:32 -0400

    Rename TS declaration

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
-declare module 'react-native-linear-gradient' {
-  import * as React from 'react';
-  import * as ReactNative from 'react-native';
+declare module "@mythologi/react-native-linear-gradient" {
+  import { Component } from "react";
+  import { ViewProps } from "react-native";
 
-  export interface LinearGradientProps extends ReactNative.ViewProps {
+  export interface LinearGradientProps extends ViewProps {
     colors: (string | number)[];
     start?: { x: number; y: number };
     end?: { x: number; y: number };
     locations?: number[];
     useAngle?: boolean;
-    angleCenter?: {x: number, y: number};
+    angleCenter?: { x: number; y: number };
     angle?: number;
   }
 
-  export class LinearGradient extends React.Component<LinearGradientProps> {}
+  export class LinearGradient extends Component<LinearGradientProps> {}
 
   export default LinearGradient;
 }
